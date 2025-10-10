3923307f0cdbacdf44f65be875ba1a0c485bb53d
Travis B. Hartwell <nafai@travishartwell.net>
2025-10-10 09:40:24 -0700

    Fix SHA256

--- Formula/myproject.rb
+++ Formula/myproject.rb
@@ -2,7 +2,7 @@ class Myproject < Formula
   desc "Project Task Runner using MyCmd"
   homepage "https://github.com/travisbhartwell/myproject"
   url "https://github.com/travisbhartwell/myproject/releases/download/v0.02/myproject-0.02.tar.gz"
-  sha256 "60e5dca48495d0e0d908b4e67661f48190141dc66f4c5df6cf38f2bd1c15dbbd"
+  sha256 "2e72d00de6ce94a83482405dbb912d169821c33165fe12b0185855a2e88d8cfa"
   license "MIT"
 
   depends_on "git"

47cddbcf8d8be2d31e90a679b77abebd8994ba8b
Travis B. Hartwell <nafai@travishartwell.net>
2025-10-10 09:31:45 -0700

    Update MyProject to 0.02

--- Formula/myproject.rb
+++ Formula/myproject.rb
@@ -1,7 +1,7 @@
 class Myproject < Formula
   desc "Project Task Runner using MyCmd"
   homepage "https://github.com/travisbhartwell/myproject"
-  url "https://github.com/travisbhartwell/myproject/releases/download/v0.01/myproject-0.01.tar.gz"
+  url "https://github.com/travisbhartwell/myproject/releases/download/v0.02/myproject-0.02.tar.gz"
   sha256 "60e5dca48495d0e0d908b4e67661f48190141dc66f4c5df6cf38f2bd1c15dbbd"
   license "MIT"
 
@@ -11,6 +11,7 @@ class Myproject < Formula
   on_macos do
     depends_on "coreutils"
     depends_on "grep"
+    depends_on "findutils"
   end
 
   def install

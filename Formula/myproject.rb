a166e508d19a3e8a5378a990404e4d92a9317baf
Travis B. Hartwell <nafai@travishartwell.net>
2025-11-03 18:05:44 -0800

    release MyProject 0.03

--- Formula/myproject.rb
+++ Formula/myproject.rb
@@ -1,7 +1,7 @@
 class Myproject < Formula
   desc "Project Task Runner using MyCmd"
   homepage "https://github.com/travisbhartwell/myproject"
-  url "https://github.com/travisbhartwell/myproject/releases/download/v0.02/myproject-0.02.tar.gz"
+  url "https://github.com/travisbhartwell/myproject/releases/download/v0.03/myproject-0.03.tar.gz"
   sha256 "2e72d00de6ce94a83482405dbb912d169821c33165fe12b0185855a2e88d8cfa"
   license "MIT"
 
